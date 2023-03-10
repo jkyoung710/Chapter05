dc097fc48bd00768ae9f70cd5897e590fbd7d0c7
기영 정 <jkyoung710@naver.com>
2023-03-10 15:30:56 +0900

    20230310 :  p 149 연습

--- IfElse/MainApp.cs
+++ IfElse/MainApp.cs
@@ -10,6 +10,48 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
+
+            Console.Write("숫자를 입력하세요 :");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+            {
+                Console.WriteLine("음수");
+            }
+
+            else if (number > 0)
+            {
+                Console.WriteLine("양수");
+                
+            }
+
+            else
+            {
+                Console.WriteLine("0");
+
+            }
+
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("짝수");
+            }
+
+            else
+            {
+                Console.WriteLine("홀수");
+            }
+
+            string result = (number % 2) == 0 ? "1" : "2";
+            Console.WriteLine(result);
+
+            Console.WriteLine(number % 2 == 0 ? "1" : "2");
+
+            // 조건 연산자 에 대한 설명 을 할줄 알아야 한다. 
+            // .
+
         }
     }
 }
