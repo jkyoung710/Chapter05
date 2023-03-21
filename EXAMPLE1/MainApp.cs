bc9b9ed018982145c6ab34952d9b4fbcb526a0d8
기영 정 <jkyoung710@naver.com>
2023-03-21 15:58:03 +0900

    20230321 : 19일에 수정하고 21에 업로드

---
+++ EXAMPLE1/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMPLE1
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+            for (int a = 0; a < 5; a++)
+            {
+                for (int b = 0; b <= a; b++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ ForEach/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[] { 0, 1, 2, 3, 4 };
+
+            foreach (int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+
+        }
+    }
+}
